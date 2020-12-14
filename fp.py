836419a78b7232c22ad59e3b33d7b2ce41ecfd3a
Jatinrajpal <jatinrajpal57@gmail.com>
2020-12-14 11:57:59 +0530

    forward propagation algo for try

---
+++ fp.py
@@ -0,0 +1,9 @@
+import numpy as np
+input_data=np.array([2,3])
+weights={'node_0':np.array([1,1]),
+		'node_1':np.array([-1,1]),
+		'output_val':np.array([2,-1])}
+node_0_value=(input_data*weights['node_0']).sum()
+node_1_value=(input_data*weights['node_1']).sum()
+print(node_1_value)
+print(node_0_value)
